682346139637a0a41d12ca480248d1950df75477
Khalid Abuhakmeh <khalid@aquabirdconsulting.com>
2019-11-30 21:28:31 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ChildProcesses
+{
+    static class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var parcel = new NpmScript();
+
+            await parcel.RunAsync(Console.WriteLine);
+
+            Console.WriteLine(parcel.HasServer
+                ? $"From ASP.NET Core. Parcel is started ({parcel.HasServer}) @ {parcel.Url} at process: {parcel.ProcessId}"
+                : "Script has executed.");
+
+            await Task.Delay(TimeSpan.FromSeconds(4));
+        }
+    }
+}
